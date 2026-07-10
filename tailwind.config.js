12c10bce807a164415a51f9a5ae921ca6f0e82b6
Raúl Cuéllar <raulito1500@gmail.com>
2026-07-09 22:23:30 -0500

    feature: new Sidebar component redesigned.

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,19 +7,24 @@ module.exports = {
     extend: {
       colors: {
         primary: {
-          50: '#f0fdfa',
-          100: '#ccfbf1',
+          50: '#ecfdf5', // Tint
+          100: '#ccfbf1', // Soft
           200: '#99f6e4',
-          300: '#5eead4',
+          300: '#5dd5c4',
           400: '#2dd4bf',
           500: '#14b8a6',
-          600: '#0d9488',
-          700: '#0f766e',
-          800: '#115e59',
+          600: '#0d9488', // Accent
+          700: '#0f766e', // Strong
+          800: '#115e59', // Ink
           900: '#134e4a',
+          950: '#042f2e',
         },
       },
+      fontSize: {
+        'xs': '11px',
+      },
     },
+
   },
   plugins: [],
 }
